c486b1327716fe6ab8a1fc6019026e447d2d913a
Matt <matt.delacruz34@gmail.com>
2023-04-03 20:29:07 -0700

    add UDP network and client handler

---
+++ lab_3/a3/network/GhostManager.java
@@ -0,0 +1,15 @@
+package a3.network;
+
+import java.util.UUID;
+
+import org.joml.Vector3f;
+
+public class GhostManager {
+
+    public void removeGhostAvatar(UUID ghostID) {
+    }
+
+    public void createGhost(UUID ghostID, Vector3f ghostPosition) {
+    }
+
+}
